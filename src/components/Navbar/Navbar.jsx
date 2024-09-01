8d80ec3458754537b18ddd922eea266768bfd6a9
Murodulla <yokubjanovich@Murodullas-MacBook-Air.local>
2024-09-01 23:36:42 +0500

    last commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,12 +4,32 @@ import AlgoritmLogo from "../../assets/svg/algoritm-logo.svg";
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div
-      className={`${styles.navbarContainer} 
+      className={`${styles.navbarContainer} ${
+        scrolled ? styles.navbarContainerScroll : ""
       }`}
     >
-      <div className={`${styles.navbarWrapper} `}>
+      <div
+        className={`${styles.navbarWrapper} ${scrolled ? styles.scrolled : ""}`}
+      >
         <a href="#">
           <img src={AlgoritmLogo} alt="main logo" />
         </a>
